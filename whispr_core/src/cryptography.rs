3521b778ab226fed1366dc9abfe6a470fa0a13be
The_good_one14 <149061222+The-good-one14@users.noreply.github.com>
2026-02-12 15:42:13 +0100

    fixed imports, fixed version issue with rand/ed25519, and added chacha20-poly1305 for encryption

--- whispr_core/src/cryptography.rs
+++ whispr_core/src/cryptography.rs
@@ -1,10 +1,10 @@
-use rand::rngs::OsRng;
+use rand::{RngCore, rngs::OsRng};
 use ed25519_dalek::{SigningKey, VerifyingKey, Verifier, Signature, Signer};
 use x25519_dalek::{EphemeralSecret, PublicKey, SharedSecret};
-use hex_literal::hex;
 use sha2::{Sha256, Digest};
 use hkdf::Hkdf;
 use crate::errors::LibError;
+use chacha20_poly1305;
 
 pub fn hash(data: &[u8]) -> [u8;32] {
     Sha256::digest(data).into()
@@ -18,6 +18,8 @@ pub fn derive_key(secret: &[u8], label: &[u8], salt: Option<&[u8]>) -> Result<[u
 }
 
 pub mod ed25519 {
+    use super::*;
+
     pub fn generate() -> (SigningKey, VerifyingKey) {
         let mut random = OsRng;
         let signing_key: SigningKey = SigningKey::generate(&mut random);
@@ -35,12 +37,28 @@ pub mod ed25519 {
 }
 
 pub mod x25519 {
+    use super::*;
     pub fn generate_ephemeral() -> (EphemeralSecret, PublicKey) {
-        let secret = EphemeralSecret::random();
+        let random = OsRng;
+        let secret = EphemeralSecret::random_from_rng(random);
         let public = PublicKey::from(&secret);
         (secret, public)
     }
     pub fn generate_shared(secret: EphemeralSecret, public: &PublicKey) -> SharedSecret {
         secret.diffie_hellman(&public)
+    }
+}
+
+pub mod crypt {
+    use super::*;
+
+    fn nonce() -> [u8;12] {
+        let mut nonce = [0u8;12];
+        OsRng.try_fill_bytes(&mut nonce).unwrap();
+        nonce
+    }
+    pub fn seal(data: &[u8], key: &[u8]) -> Vec<u8> {
+        let nonce: [u8; 12] = nonce();
+        Vec::new()
     }
 }
