f8bb6ce0455af1d95bd9217b9f559fac08170812
The-good-one14 <149061222+The-good-one14@users.noreply.github.com>
2026-02-12 10:52:37 +0100

    added cryptographic functions for authentication via ed25519, and key exchange via x25519 and a kdf. also added required libraries in cargo.toml

--- whispr_core/src/cryptography.rs
+++ whispr_core/src/cryptography.rs
@@ -1,17 +1,46 @@
 use rand::rngs::OsRng;
-use ed25519_dalek::{SigningKey, VerifyingKey, Verifier, Signature};
+use ed25519_dalek::{SigningKey, VerifyingKey, Verifier, Signature, Signer};
+use x25519_dalek::{EphemeralSecret, PublicKey, SharedSecret};
+use hex_literal::hex;
+use sha2::{Sha256, Digest};
+use hkdf::Hkdf;
+use crate::errors::LibError;
 
-pub fn create_keypair() -> (signing_key, verifying_key) {
-    let mut random = OsRng;
-    let signing_key: SigningKey = SigningKey::generate(&mut random);
-    let verifying_key: VerifyingKey = signing_key.verifying_key();
-    (signing_key, verifying_key)
+pub fn hash(data: &[u8]) -> [u8;32] {
+    Sha256::digest(data).into()
 }
 
-pub fn sign_data(data: &[u8], signing_key: &SigningKey) -> Signature {
-    let signature: Signature = signing_key.sign(message);
+pub fn derive_key(secret: &[u8], label: &[u8], salt: Option<&[u8]>) -> Result<[u8;32],LibError> {
+    let kdf = Hkdf::<Sha256>::new(salt, secret);
+    let mut output = [0u8;32];
+    kdf.expand(label, &mut output).map_err(|_| LibError::KeyLengthError)?;
+    Ok(output)
 }
 
-pub fn verify_data(data: &[u8] , signature: &Signature, verifying_key: &VerifyingKey) -> bool {
-    verifying_key.verify(data, &signature).is_ok()
+pub mod ed25519 {
+    pub fn generate() -> (SigningKey, VerifyingKey) {
+        let mut random = OsRng;
+        let signing_key: SigningKey = SigningKey::generate(&mut random);
+        let verifying_key: VerifyingKey = signing_key.verifying_key();
+        (signing_key, verifying_key)
+    }
+
+    pub fn sign_data(data: &[u8], signing_key: &SigningKey) -> Signature {
+        signing_key.sign(data)
+    }
+
+    pub fn verify_data(data: &[u8] , signature: &Signature, verifying_key: &VerifyingKey) -> bool {
+        verifying_key.verify(data, &signature).is_ok()
+    }
+}
+
+pub mod x25519 {
+    pub fn generate_ephemeral() -> (EphemeralSecret, PublicKey) {
+        let secret = EphemeralSecret::random();
+        let public = PublicKey::from(&secret);
+        (secret, public)
+    }
+    pub fn generate_shared(secret: EphemeralSecret, public: &PublicKey) -> SharedSecret {
+        secret.diffie_hellman(&public)
+    }
 }
