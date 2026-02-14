dabe858d070c5dccbd79b58c83c168f917be1784
The-good-one14 <149061222+The-good-one14@users.noreply.github.com>
2026-02-15 00:18:34 +0100

    added serde and redid some of models.rs

--- whispr_core/src/models.rs
+++ whispr_core/src/models.rs
@@ -1,8 +1,22 @@
+pub enum LibError {
+    KeyLengthError(Option<String>),
+    EncryptionError(Option<String>),
+    DecryptionError(Option<String>),
+    NonceError(Option<String>),
+    RandomNumberError,
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
 pub struct Message {
     pub sender_hash: [u8;32],
     pub reciever_hash: [u8;32],
     pub public_key: [u8;32],
     pub nonce: [u8;12],
-    pub payload: Vec<u8>,
+    pub payload: Vec<u8>
+}
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct Envelope {
+    pub message: Message,
+    #[serde(with = "serde_bytes")]
     pub signature: [u8;64]
 }
