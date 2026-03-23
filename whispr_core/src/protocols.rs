1443a5601ecba45dba5a113b5e4de6d513815210
The-good-one14 <149061222+The-good-one14@users.noreply.github.com>
2026-03-23 22:30:53 +0100

    changed an error with fingerprint, and added variable types for safety/readability

--- whispr_core/src/protocols.rs
+++ whispr_core/src/protocols.rs
@@ -6,7 +6,7 @@ use x25519_dalek::{PublicKey, StaticSecret};
 pub fn get_identity(private: SigningKey) -> Result<Identity, LibError> {
     let static_keys: (StaticSecret, PublicKey) = generate_static(derive_key(private.as_bytes(), &ENCRYPTION_LABEL, None)?);
     Ok(Identity {
-        fingerprint: hash(private.as_bytes()),
+        fingerprint: hash(get_public(&private).as_bytes()),
         public: get_public(&private),
         x25519_private: static_keys.0,
         x25519_public: static_keys.1,
@@ -14,12 +14,12 @@ pub fn get_identity(private: SigningKey) -> Result<Identity, LibError> {
     })
 }
 
-pub fn seal_n_sign(message: &[u8], reciever: [u8;32], identity: Identity, publickey: &PublicKey) -> Result<Envelope, LibError> {
+pub fn seal_n_sign(message: &[u8], reciever: [u8;32], identity: &Identity, publickey: &PublicKey) -> Result<Envelope, LibError> {
     let session: Session = generate_ephemeral();
-    let shared = generate_shared(crate::models::SecretKeyType::EphemeralSecret(session.secret), publickey);
+    let shared: x25519_dalek::SharedSecret = generate_shared(crate::models::SecretKeyType::EphemeralSecret(session.secret), publickey);
     let key: [u8;32] = derive_key(shared.as_bytes(), &ENCRYPTION_LABEL, None)?;
-    let nonce = generate_nonce()?;
-    let sealed_payload = seal(&message, &key, &nonce)?;
+    let nonce: [u8; 12] = generate_nonce()?;
+    let sealed_payload: Vec<u8> = seal(message, &key, &nonce)?;
     let sealed_message: Message = Message {
         sender_hash: identity.fingerprint,
         reciever_hash: reciever,
@@ -27,7 +27,7 @@ pub fn seal_n_sign(message: &[u8], reciever: [u8;32], identity: Identity, public
         nonce,
         payload: sealed_payload
     };
-    let sealed_message_bytes: Vec<u8> = postcard::to_stdvec(&sealed_message).map_err(|e| LibError::SerializationError(Some(e.to_string())))?;
+    let sealed_message_bytes: Vec<u8> = postcard::to_stdvec(&sealed_message).map_err(|e| LibError::SerializationError(e.to_string()))?;
     let signature: [u8; 64] = sign_data(&sealed_message_bytes, &identity.private).to_bytes();
     Ok(Envelope {
         message: sealed_message_bytes,
@@ -39,8 +39,7 @@ pub fn open_n_verify(envelope: Envelope, identity: &Identity, public_key: &Verif
     if !verify_data(&envelope.message, &Signature::from_bytes(&envelope.signature), &public_key) {
         return Err(LibError::BadSignature);
     }
-    let message: Message = postcard::from_bytes(&envelope.message).map_err(|e| LibError::DecryptionError(Some(e.to_string())))?;
+    let message: Message = postcard::from_bytes(&envelope.message).map_err(|e| LibError::DeserializationError(e.to_string()))?;
     let key: [u8; 32] = derive_key(generate_shared(SecretKeyType::StaticSecret(identity.x25519_private.clone()), &PublicKey::from(message.public_key)).as_bytes(), &ENCRYPTION_LABEL, None)?;
     Ok(open(&message.payload, &key, &message.nonce)?)
-
 }
